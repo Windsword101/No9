7166b8292b1b7db95a2961766f1e9c94471a7cba
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-06-15 15:32:40 +0800

    changescene

--- No9/Assets/C#Script/ChangeScene.cs
+++ No9/Assets/C#Script/ChangeScene.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class ChangeScene : MonoBehaviour
 {
     public string toScene;
-    
+    private string prevScene = "prevScene";
+
+    private void Start()
+    {
+        PlayerPrefs.SetString(prevScene, Application.loadedLevelName);
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Z))
