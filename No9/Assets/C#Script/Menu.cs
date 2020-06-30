4b1a45ea5fb55726b7e4ebfbbc9b2751978b545e
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-07-01 00:42:58 +0800

    ScreenSize

--- No9/Assets/C#Script/Menu.cs
+++ No9/Assets/C#Script/Menu.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [Header("解析度下拉選單")]
+    public Dropdown ScreenSizeDropdown;
     public void NextScene()
     {
         Application.LoadLevel("No9");
@@ -12,5 +15,22 @@ public class Menu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Update()
+    {
+        switch (ScreenSizeDropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 720, false);
+                break;
+            case 2:
+                Screen.SetResolution(800, 480, false);
+                break;
+        }
+        
     }
 }
