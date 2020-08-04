8cc76974d376f4f08b956f0cb5241346d169ec5b
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2020-08-04 16:32:23 +0800

    001

--- No9/Assets/C#Script/IntoDoor.cs
+++ No9/Assets/C#Script/IntoDoor.cs
@@ -1,16 +1,17 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntoDoor : MonoBehaviour
 {
-    public GameObject IntoRoom;
+    public GameObject Survey;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag=="Player")
         {
-            IntoRoom.SetActive(true);
+            Survey.SetActive(true);
         }
     }
 
@@ -18,9 +19,9 @@ public class IntoDoor : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            IntoRoom.SetActive(false);
+            Survey.SetActive(false);
         }
     }
 
 
-}*/
+}
